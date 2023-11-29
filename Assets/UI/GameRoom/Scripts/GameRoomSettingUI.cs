2ec8c1a7d54fcb7bc963fa87a5fbe1c50eb81e24
soohyun <tngusrla0812@gmail.com>
2023-11-29 20:11:55 +0900

    (6) 대기실 꾸미기와 UI 배치

---
+++ Assets/UI/GameRoom/Scripts/GameRoomSettingUI.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameRoomSettingUI : SettingUI
+{
+    public void ExitGameRoom()
+    {
+        var manager = AmongUsRoomManager.singleton;
+        if(manager.mode == Mirror.NetworkManagerMode.Host)
+        {
+            manager.StopHost();
+        }
+        else if (manager.mode == Mirror.NetworkManagerMode.ClientOnly)
+        {
+            manager.StopClient();
+        }
+    }
+}
