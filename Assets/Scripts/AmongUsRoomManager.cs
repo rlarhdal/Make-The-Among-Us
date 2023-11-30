82452a0e9af52aaf9f086cb1edb8ecab148d117f
soohyun <tngusrla0812@gmail.com>
2023-11-30 20:15:01 +0900

    (7) 대기실 캐릭터 구현하기

--- Assets/Scripts/AmongUsRoomManager.cs
+++ Assets/Scripts/AmongUsRoomManager.cs
@@ -5,5 +5,11 @@ using Mirror;
 
 public class AmongUsRoomManager : NetworkRoomManager
 {
-   
+    public override void OnRoomServerConnect(NetworkConnectionToClient conn)
+    {
+        base.OnRoomServerConnect(conn);
+
+        var player = Instantiate(spawnPrefabs[0]);
+        NetworkServer.Spawn(player, conn);
+    }
 }
